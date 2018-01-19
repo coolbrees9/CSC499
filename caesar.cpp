37f4ba56933bea1d8d35dea77014964123cd8f71
Colin Oneal <oneal_c@cobra.cs.mercer.edu>
2018-01-19 11:24:09 -0500

    Initial commit

---
+++ caesar.cpp
@@ -0,0 +1,27 @@
+/*  Colin O'Neal
+ *  Caesar Cipher
+ *  CSC 499
+ *  5/4/2018      */
+
+#include <cstdlib>
+#include <math.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+string encrypt();
+string decrypt();
+int main()
+{
+      printf("Hello World\n");
+      encrypt();
+      decrypt();
+}
+string encrypt()
+{
+      return "HI";
+}
+string decrypt()
+{
+      return "NO";
+}
